ea685b5bdea8bcb4459e83d4247387e9f320cd83
Tracie Stampley <tracie.stampley@daugherty.com>
2021-01-07 09:17:31 -0600

    add state

--- typerace/src/App.js
+++ typerace/src/App.js
@@ -1,17 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Button from './Button';
 
-function App() {
-  const buttonTextItems = ['Click Me!', 'No Me!', 'Please no clicky']
-  return (
-    <div>
-      <h2>Type Race</h2>
-      <input />
-      <hr/>
-      {buttonTextItems.map(textItem => <Button buttonText={textItem} />)}
-    </div>
-  );
-}
+const buttonTextItems = ['Click Me!', 'No Me!', 'Please no clicky'];
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      userText: ''
+    };
+  };
+
+  updateUserText = event => {
+    this.setState({ userText: event.target.value });
+  };
+
+  render () {
+    return (
+      <div>
+        <h2>Type Race</h2>
+        <input value={this.state.userText} onChange={this.updateUserText} />
+        <hr/>
+        {buttonTextItems.map(textItem => <Button buttonText={textItem} />)}
+      </div>
+    );
+  };
+};
 
 export default App;
